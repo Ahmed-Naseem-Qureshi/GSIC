d5042e5a7abef13f1a41132e0b27e65108145831
Ahmed-Naseem-Qureshi <160408562+Ahmed-Naseem-Qureshi@users.noreply.github.com>
2024-02-19 13:22:06 +0500

    Add files via upload

---
+++ program-04/main.js
@@ -0,0 +1,3 @@
+var famous_person = "Martin Fowler";
+var message = ` once said, "Any fool can write code that a computer can understand. Good programmers write code that humans can understand."`;
+console.log(famous_person + message);
