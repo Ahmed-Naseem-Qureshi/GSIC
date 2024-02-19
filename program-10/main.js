661eb62496bc326780d8fa101f458d56ee7ad452
Ahmed-Naseem-Qureshi <160408562+Ahmed-Naseem-Qureshi@users.noreply.github.com>
2024-02-19 15:05:07 +0500

    Add files via upload

---
+++ program-10/main.js
@@ -0,0 +1,5 @@
+var friends = ["Fasih", "Salman", "Maaz"];
+
+for(var i = 0; i < friends.length; i++) {
+    console.log("Aur " + friends[i] + " bhai kya haal hain");
+}
---
+++ program-11/main.js
@@ -0,0 +1,7 @@
+
+var favoriteCars = ["Tesla Model S", "Honda Civic", "Ford Mustang", "BMW M3", "Toyota Prius"];
+
+
+for (var i = 0; i < favoriteCars.length; i++) {
+    console.log("I would like to own a " + favoriteCars[i] + ".");
+}
