1a2d39a2995a600681023592ac4fee0d1e3f4a90
Ahmed-Naseem-Qureshi <160408562+Ahmed-Naseem-Qureshi@users.noreply.github.com>
2024-02-19 19:31:02 +0500

    Add files via upload

---
+++ program-18/main.js
@@ -0,0 +1,5 @@
+var programmingLanguages = ["JavaScript", "Python", "Java", "C++", "Ruby", "Swift", "Go", "TypeScript"];
+
+for (var i = 0; i < programmingLanguages.length; i++) {
+    console.log("- " + programmingLanguages[i]);
+}
