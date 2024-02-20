846c52161007c2a2bd8d810eb68bd36582760e99
Ahmed-Naseem-Qureshi <160408562+Ahmed-Naseem-Qureshi@users.noreply.github.com>
2024-02-20 16:40:49 +0500

    Update main.js

--- program-35/main.js
+++ program-35/main.js
@@ -1,7 +1,7 @@
-function make_shirt(city, country) {
+function describe_city()(city, country) {
     console.log(city + " is in " + country);
 }
 
-make_shirt("Karachi", "Pakistan");
-make_shirt("Tokyo", "Japan");
-make_shirt("Lahore", "Pakistan");
+describe_city()("Karachi", "Pakistan");
+describe_city()("Tokyo", "Japan");
+describe_city()("Lahore", "Pakistan");
