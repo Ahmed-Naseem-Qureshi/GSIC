17ddaf9530435a3e17ef5fa170295f33def76dfd
Ahmed-Naseem-Qureshi <160408562+Ahmed-Naseem-Qureshi@users.noreply.github.com>
2024-02-20 16:54:15 +0500

    Add files via upload

---
+++ program-38/main.js
@@ -0,0 +1,9 @@
+var magicians = ["Ali", "Shamil", "Jadoo"];
+
+function show_magicians() {
+    for (let i = 0; i < magicians.length; i++) {
+        console.log("Magician " + magicians[i]);
+    }
+}
+
+show_magicians();
