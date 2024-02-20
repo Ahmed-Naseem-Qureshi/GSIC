f701adc4853d5a1db2f118033e9a6bfc1e8a6095
Ahmed-Naseem-Qureshi <160408562+Ahmed-Naseem-Qureshi@users.noreply.github.com>
2024-02-20 13:55:06 +0500

    Add files via upload

---
+++ program-20/main.js
@@ -0,0 +1,42 @@
+let car = 'subaru';
+let number = 42;
+
+// Test 1
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+
+// Test 2
+console.log("Is car == 'honda'? I predict False.");
+console.log(car == 'honda');
+
+// Test 3
+console.log("Is number === 42? I predict True.");
+console.log(number === 42);
+
+// Test 4
+console.log("Is number != 42? I predict False.");
+console.log(number != 42);
+
+// Test 5
+console.log("Is car !== 'Toyota'? I predict True.");
+console.log(car !== 'Toyota');
+
+// Test 6
+console.log("Is car.length < 5? I predict False.");
+console.log(car.length < 5);
+
+// Test 7
+console.log("Is car.startsWith('sub')? I predict True.");
+console.log(car.startsWith('sub'));
+
+// Test 8
+console.log("Is number >= 50? I predict False.");
+console.log(number >= 50);
+
+// Test 9
+console.log("Is car.includes('aru')? I predict True.");
+console.log(car.includes('aru'));
+
+// Test 10
+console.log("Is number % 2 === 0? I predict True.");
+console.log(number % 2 === 0);
