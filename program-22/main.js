e72ce2ac3b08bd304bbfad4c935c6d1a363f5f0c
Ahmed-Naseem-Qureshi <160408562+Ahmed-Naseem-Qureshi@users.noreply.github.com>
2024-02-20 14:02:34 +0500

    Add files via upload

---
+++ program-22/main.js
@@ -0,0 +1,10 @@
+// Alien color assigned as 'green'
+var alien_color = 'green';
+
+// If statement to test whether the alien's color is green
+if (alien_color === 'green') {
+    // Print a message if the condition is true
+    console.log("Congratulations! You just earned 5 points.");
+}
+
+
---
+++ program-22/main1.js
@@ -0,0 +1,8 @@
+// Alien color assigned as 'red'
+var alien_color = 'red';
+
+// If statement to test whether the alien's color is green
+if (alien_color === 'green') {
+    // There will be no output if the condition is false
+    console.log("Congratulations! You just earned 5 points.");
+}
