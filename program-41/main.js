8e11b4e8935984462f6984a627f4f9feaa93569b
Ahmed-Naseem-Qureshi <160408562+Ahmed-Naseem-Qureshi@users.noreply.github.com>
2024-02-20 17:32:18 +0500

    Update main.js

--- program-41/main.js
+++ program-41/main.js
@@ -1,13 +1,9 @@
-function orderSandwich(...items) {
-    if (items.length === 0) {
-        console.log("You ordered an empty sandwich. Please choose some items.");
-    } else {
-        console.log("Items on the sandwich: " + items.join(', '));
-    }
+function create_sandwich(...items) {
+     console.log("Items on the sandwich: " + items.join(', '));
     console.log("-----------------------------");
 }
 
 // Call the function with different numbers of arguments
-orderSandwich("Ham", "Cheese", "Lettuce");
-orderSandwich("Turkey", "Swiss", "Tomato", "Mustard");
-orderSandwich(); // Empty sandwich
+create_sandwich("Beef", "Cheese", "Lettuce");
+create_sandwich("Chicken", "Ketchup", "Tomato", "Mustard");
+create_sandwich("Potato", "Ketchup");
