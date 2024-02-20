899782dd19c1fd8ca704d42ddd888b56e221f149
Ahmed-Naseem-Qureshi <160408562+Ahmed-Naseem-Qureshi@users.noreply.github.com>
2024-02-20 16:20:39 +0500

    Add files via upload

---
+++ program-31/main.js
@@ -0,0 +1,7 @@
+var pizza = ["Fajita", "Barbecue", "Cheese"];
+
+for (var i = 0; i < pizza.length; i++) {
+    console.log("I like " + pizza[i] + " pizza");
+}
+
+console.log("\nI like pizza too much.\nMy favourite pizaa flavours are Fajita, Barbecue and Cheese.\nI really love pizza.");
