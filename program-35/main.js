b91563ddde0856a3ecc252f64c88281aff23b43b
Ahmed-Naseem-Qureshi <160408562+Ahmed-Naseem-Qureshi@users.noreply.github.com>
2024-02-20 16:38:44 +0500

    Add files via upload

---
+++ program-35/main.js
@@ -0,0 +1,7 @@
+function make_shirt(city, country) {
+    console.log(city + " is in " + country);
+}
+
+make_shirt("Karachi", "Pakistan");
+make_shirt("Tokyo", "Japan");
+make_shirt("Lahore", "Pakistan");
